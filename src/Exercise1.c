7c4ea7723fe336aab2ee85d1cf336586a4b55a78
quynhnguyen2901 <91877086+quynhnguyen2901@users.noreply.github.com>
2021-10-04 19:19:41 +0700

    update ex1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -16,6 +16,12 @@ int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	int testcase = atoi(argv[1]);
 	//Your codes here
-	
+	int digit;
+	while (testcase!=0)
+	{
+		digit=testcase%10;
+		printf("%d",digit);
+		testcase=(testcase-digit)/10;
+	}
 	return 0;
 }
